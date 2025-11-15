d5093e427548bc1509015c5d6e4153372f9850a5
Brayan <brayanjoseazofeifaamdor02@gmail.com>
2025-11-14 22:12:36 -0600

    IgestorMemoria
    Se crea una interfaz con metodos para agregar elimanar buscar lista cartones, tombola y tablero

---
+++ src/Modelo/IGestorMemoria.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author Braya
+ */
+public interface IGestorMemoria {
+    void agregarCarton(Carton carton);
+    void eliminarCarton(Carton id);
+    List<Carton> obtenerCartones();
+    Carton obtenerCarton(String id);
+    Tablero obtenerTablero();
+    Tombola obtenerTombola();
+    void reiniciarJuego();
+}
