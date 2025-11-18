bf526f8adfb495b39a2ca8773a2e472321924c00
Kevinq <kevinquiros.m@gmail.com>
2025-11-17 19:08:34 -0600

    Ajuste Bingo
    Limpieza de lineas en blanco

--- src/Vista/Bingo.java
+++ src/Vista/Bingo.java
@@ -25,14 +25,12 @@ public class Bingo extends javax.swing.JFrame {
     }
     
         private void configurarComboBoxes() {
-        // Configurar combobox de modo de juego
         String[] modos = new String[EnumModoJuego.values().length];
         for (int i = 0; i < EnumModoJuego.values().length; i++) {
             modos[i] = EnumModoJuego.values()[i].getModoJuego();
         }
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(modos));
         
-        // Configurar combobox de tipo de juego
         String[] tipos = new String[EnumTipoJuego.values().length];
         for (int i = 0; i < EnumTipoJuego.values().length; i++) {
             tipos[i] = EnumTipoJuego.values()[i].getTipoJuego();
