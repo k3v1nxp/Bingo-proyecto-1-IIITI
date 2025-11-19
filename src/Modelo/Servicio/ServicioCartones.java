8065f91258d7f70a7d798967d4197d4727ae875f
Kevinq <kevinquiros.m@gmail.com>
2025-11-19 11:32:41 -0600

    Se crea servicioCartones
    Solo con el gestor de memoria

---
+++ src/Modelo/Servicio/ServicioCartones.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.Servicio;
+
+import Modelo.Carton;
+import Modelo.EnumModoJuego;
+import Modelo.GestorMemoria;
+import Modelo.ICartonFactory;
+
+/**
+ *
+ * @author kevin
+ */
+public class ServicioCartones {
+    private GestorMemoria repositorio;
+
+    public ServicioCartones(GestorMemoria repositorio) {
+        this.repositorio = repositorio;
+    }
+    
+}
